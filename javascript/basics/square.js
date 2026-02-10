0d428f467b9bd3ad368c85b6fe59df02c44e3c42
Maia <Maiakbard@gmail.com>
2026-02-10 15:13:33 -0600

    Add square function to library

---
+++ javascript/basics/square.js
@@ -0,0 +1,20 @@
+// SQUARE
+// Problem: Returns the square of the inputted number
+// Returns: Number multiplied by itself
+
+const square = (value) => {
+  return value * value
+}
+
+// Examples:
+console.log(square(5)) // 25
+
+// Key concepts:
+// - Squaring a number (value * value)
+// - Alternative: Math.pow(value, 2) or value ** 2
+// - Returns a number
+
+// When to use:
+// - Calculationg area
+// - Mathematical operations
+// - Distance calculations
