34ebc8ad069bcc71da371923fffb746f05c5afe8
Maia <Maiakbard@gmail.com>
2026-02-10 15:09:47 -0600

    Add multiply function to library

---
+++ javascript/basics/multiply.js
@@ -0,0 +1,20 @@
+// MULTIIPLY
+// Problem: Multiplies the two inpputted numbers together
+// Returns: Product of two numbers
+
+const multiply = (val1, val2) => {
+  return val1 * val2
+}
+
+// Examples:
+console.log(multiply(1, 2)) // 2
+console.log(multiply(2, 2)) // 4
+
+// Key concepts:
+// - Multiplication operator (*)
+// - Two parameters
+// - Returns a number
+
+// When to use:
+// - Multiplying two values
+// - Scaling values
