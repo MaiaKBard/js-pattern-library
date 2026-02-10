ea7b9313b6a265d3e227256e5cbfa1b344ca3494
Maia <Maiakbard@gmail.com>
2026-02-10 15:05:38 -0600

    Add function add to pattern library

---
+++ javascript/basics/add.js
@@ -0,0 +1,19 @@
+// ADD
+// Problem: Adds the two inputted numbers together
+// Returns: Sum of two numbers
+
+const add = (num1, num2) => {
+  return num1 + num2
+}
+
+// Examples:
+console.log(add(5, 10)) // 15
+
+// Key concepts:
+// - Basics arithmetic operator (+)
+// - Two parameters
+// - Returns a number
+
+// When to use:
+// - Adding two values
+// - Basics calculations
